bfee18d3436b3bbbcf9352a812de2bd4624789a5
Anton Lundblad <anton.lundblad37@gmail.com>
2025-03-03 11:33:52 +0100

    hej

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Navbar from './komponenter/navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+// export NODE_OPTIONS=--openssl-legacy-provider, use before npm start
+import sida1 from './sidor/sida1';
+import sida2 from './sidor/sida2';
+import sida3 from './sidor/sida3';
+
+
 
 function App() {
   return (
@@ -9,7 +15,10 @@ function App() {
       <Router>
         <Navbar /> {/* Corrected Navbar component */}
         <Routes>
-          <Route path='/' element={<h1>Hem</h1>} />
+          <Route path='/' element={<h1> hem</h1>} />
+        <Route path="/page1" element={<sida1 />} />
+        <Route path="/page2" element={<sida2 />} />
+        <Route path="/page3" element={<sida3 />} />
         </Routes>
       </Router>
     </>
---
+++ src/sidor/sida1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function sida1() {
+  return (
+    <div>
+      <h1>Page One</h1> 
+      <p>Innehåll för sida 1...</p>
+    </div>
+  );
+}
+
+export default sida1;
