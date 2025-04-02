a0fa152035a81e1018db8324ca17636f87e27ecc
Anton Lundblad <anton.lundblad37@gmail.com>
2025-04-02 15:32:38 +0200

    hhh

--- src/komponenter/information.js
+++ src/komponenter/information.js
@@ -1,33 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './information.css';
-import InfoSection from './info'; // lägg till Infosection ifall du vill ha blobbar :) 
 
+function Information() {
+  return (
+    <div id="contact" className="section">
+      <h1>Information</h1>
+      <div className="subheadings">
+        <div className="subheading-container">
+          <h2 className="subheading">PET-värde</h2>
+          <p className="subtext">
+          PET-skalan är snarlik den allmänna celsiusskalan, men baseras på flera olika meteorologiska
+mått så som lufttemperatur, vindhastighet, lufttryck och luftfuktighet. Därmed modellerar
+PET olika väderförhållandens effekt på den faktiska temperatur som påverkar kroppens
+fysiologiska funktioner. Exempelvis kan värme uppfattas som mer intensiv vid hög luft-
+fuktighet än vid torrt väder. Vid Göteborgsvarvet har distans, tid och antal deltagare varit
+relativt konstanta över åren. Därmed har PET-värdets variation visat sig vara den starkast
+bidragande faktorn till antalet löpare som kollapsar.
+          </p>
+        </div>
 
-function Information () {
-    return(
-    <>
-        <div id="contact" className="section">
-          <h1>Information</h1>
-          <div className="subheadings">
-            <div className="left">
-              <h2 className="subheading">PET-värde</h2>
-              <p className="subtext">
-                Ett mått på fysisk prestation kopplat till syreupptagningsförmåga.
-                Hundar har i tusentals år varit människans trogna följeslagare. Från att ha varit vilda rovdjur till att bli våra sällskapsdjur, har hundar utvecklats till att spela många olika roller i människans liv. De fungerar inte bara som familjemedlemmar, utan också som arbetande djur – exempelvis som vakthundar, ledarhundar, räddningshundar och terapihundar. Alla tamhundar härstammar från vargen, och genom selektiv avel har vi fått fram de hundraser vi känner till idag. Det finns över 300 olika raser, alla med olika egenskaper, temperament och utseende. Från den lilla chihuahuan till den stora grand danois – varje ras har sitt unika syfte och sin historia.
-              </p>
-            </div>
-            <div className="right">
-              <h2 className="subheading">Ansträngningsutlöst kollaps</h2>
-              <p className="subtext">
-              En plötslig kollaps som uppstår vid eller efter fysisk ansträngning.
-              Alla tamhundar härstammar från vargen, och genom selektiv avel har vi fått fram de hundraser vi känner till idag. Det finns över 300 olika raser, alla med olika egenskaper, temperament och utseende. Från den lilla chihuahuan till den stora grand danois – varje ras har sitt unika syfte och sin historia. Alla tamhundar härstammar från vargen, och genom selektiv avel har vi fått fram de hundraser vi känner till idag. Det finns över 300 olika raser, alla med olika egenskaper, temperament och utseende. Från den lilla chihuahuan till den stora grand danois – varje ras har sitt unika syfte och sin historia.
-            </p>
-          </div>
+        <div className="subheading-container">
+          <h2 className="subheading">Ansträngningsutlöst kollaps</h2>
+          <p className="subtext">
+          Antalet kollapsande deltagare under ett lopp kan dock inte endast tillskrivas väderförhållan-
+den, utan även löparens erfarenhet, inställning och förberedelse spelar in. Under Stockholm
+Marathon 2024 behövde en mindre andel löpare ambulansupphämtning, trots högre tempe-
+raturer än under Göteborgsvarvet. En förklaring kan vara att halvmaraton lockar fler
+amatörlöpare, medan maratonlöpare ofta är mer erfarna och beredda på de fysiska ansträng-
+ningarna. Studier visar exempelvis att tävlingslöpare har en bättre förmåga att anpassa
+sin hastighet för att justera kroppens energinivå innan kroppstemperaturen blir för hög.
+          </p>
+        </div>
+
+        <div className="subheading-container">
+          <h2 className="subheading">Vad är vårat mål med sprida denna information?</h2>
+          <p className="subtext">
+          Vårt mål är att öka kunskapen om hur väderförhållanden påverkar fysisk prestation och hälsorisker vid löpning. Genom att analysera samband mellan temperatur, ansträngning och kollapser under lopp kan vi bidra till bättre förberedelse, minskade medicinska incidenter och en säkrare löpupplevelse för alla deltagare.
+          </p>
+        </div>
       </div>
-  </div>
-
-    </>
-
-    );
+      {/* <div className="image-container">
+      <video autoPlay loop muted className="info-image">
+          <source src="/lopningvid.mp4" type="video/mp4" />
+        </video>
+      </div> */}
+    </div>
+    
+  );
 }
+
 export default Information;
+
