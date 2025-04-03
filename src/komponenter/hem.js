ef64b0f4c5f517111d52a0a64c801143dbecb1aa
EdithKjellberg <edith.kjellberg00@gmail.com>
2025-04-03 09:58:09 +0200

    hejs

--- src/komponenter/hem.js
+++ src/komponenter/hem.js
@@ -8,7 +8,7 @@ const Home = () => {
     <div div id="home" className="video-container">
       {/* Bakgrundsvideo */}
       <div className="video-background">
-        <video autoPlay loop muted>
+        <video autoplay muted playsinline>
           <source src="/Untitled.mp4" type="video/mp4" />
         </video>
       </div>
