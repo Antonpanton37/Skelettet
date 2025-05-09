7655634c2929bf50fc47efc54ece1f84d1772450
Anton Lundblad <anton.lundblad37@gmail.com>
2025-05-09 10:33:20 +0200

    fix

--- src/komponenter/navbar.js
+++ src/komponenter/navbar.js
@@ -59,6 +59,11 @@ function Navbar() {
                   Om oss
                 </Link>
               </li>
+              <li className='nav-item'>
+                <Link to="rad" smooth={true} duration={500} className='nav-links' onClick={closeMobileMenu}>
+                  Litteratur
+                </Link>
+              </li>
            
             </ul>
             </div>
--- src/komponenter/rad.js
+++ src/komponenter/rad.js
@@ -4,9 +4,36 @@ import './rad.css';
 function Rad() {
   return (
     <div id="rad" className="section">
-    <h1>
-        Här lägger vi in referenser
-    </h1>
+      <h1>Litteratur</h1>
+      <ol className="reference-list">
+        <li>
+        P. Höppe, ”The physiological equivalent temperature - a universal index for the
+biometeorological assessment of the thermal environment,” nternational Journal of
+Biometeorology, årg. 43, nr 2, s. 71–75, aug. 2011, doi: 10.1007/s004840050118.
+        </li>
+        <li>
+        S. Thorsson m. fl., ”Is Physiological Equivalent Temperature (PET) a superior
+screening tool for heat stress risk than Wet-Bulb Globe Temperature (WBGT)
+index? Eight years of data from the Gothenburg half marathon,” British Journal of
+Sports Medicine, årg. 55, nr 14, s. 825–830, Maj 2020, ”doi: 10.1136/bjsports-2019-
+100632”.
+        </li>
+        <li>
+        J. Lundberg m. fl., Två hjärtstopp och över 40 kollapser under Göteborgsvarvet,
+Göteborgs-Posten, 18 Maj 2024, [Online] Tillgänglig: https://www.gp.se/sport/tva-
+hjartstopp-och-over-40-kollapser-under-goteborgsvarvet-.bf8967e9-a236-4eac-846f-ce0cd7e2cfad,
+Hämtad: 2025-01-29.
+        </li>
+        <li>
+        M. Schwellnus m. fl., ”Prerace Medical Screening and Education Reduce Medical
+Encounters in Distance Road Races: SAFER VIII Study in 153 208 Race Star-
+ters,” British Journal of Sports Medicine, årg. 53, nr 10, s. 634–39, Maj. 2019, doi:
+https://doi.org/10.1136/bjsports-2018-099275.
+        </li>
+        <li>
+        H. Lüning, privat kommunikation, Feb. 2025.
+        </li>
+      </ol>
     </div>
   );
 }
