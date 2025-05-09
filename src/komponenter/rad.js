dcb3f91924ca8e63249257354abbd7fdb371699e
Anton Lundblad <anton.lundblad37@gmail.com>
2025-05-09 10:44:00 +0200

    aaa

--- src/komponenter/rad.js
+++ src/komponenter/rad.js
@@ -33,6 +33,12 @@ https://doi.org/10.1136/bjsports-2018-099275.
         <li>
         H. Lüning, privat kommunikation, Feb. 2025.
         </li>
+        <p className="thank-you-text">
+        Vi vill rikta ett stort tack till Hampus Lüning för hans stora engagemang och expertis inom området. 
+      </p>
+      <p className="thank-you-text">
+        Vi vill även passa på att tacka Nils Bredin och Tilde Johansson för att vara våra frontfigurer. 
+        </p>
       </ol>
     </div>
   );
