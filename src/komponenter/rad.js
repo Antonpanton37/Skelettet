055c9acc67121e7adac20d7bc0583afed1d49c73
EdithKjellberg <edith.kjellberg00@gmail.com>
2025-05-15 16:46:51 +0200

    jhg

--- src/komponenter/rad.js
+++ src/komponenter/rad.js
@@ -7,32 +7,59 @@ function Rad() {
       <h1>Litteratur</h1>
       <ol className="reference-list">
         <li>
+        E. Carlström m. fl., ”Medical Emergencies During a Half Marathon Race - The
+Influence of Weather,” International Journal of Sports Medicine, årg. 40, nr 5,
+s. 312–16, Maj 2019, doi: 10.1055/a-0835-6063.
+
+        </li>
+        <li>
+        C. A. Asplund m. fl., ”Exercise-Associated Collapse: An Evidence-Based Review and
+Primer for Clinicians,” British Journal of Sports Medicine, årg. 45, nr 14, s. 1157–62,
+nov. 2011, doi: 10.1136/bjsports-2011-090378.
+
+        </li>
+        <li>
+        S. Thorsson m. fl., ”Is Physiological Equivalent Temperature (PET) a Superior Scre-
+ening Tool for Heat Stress Risk than Wet-Bulb Globe Temperature (WBGT) Index?
+Eight Years of Data from the Gothenburg Half Marathon,” British Journal of Sports
+Medicine, årg. 55, nr 15, s. 825–30, aug. 2021, doi: https://doi.org/10.1136/bjsports-
+2019-100632.
+
+        </li>
+        <li>
+        A. Khorram-Manesh m. fl., ”Profiling Collapsing Half Marathon Runners-Emerging
+Risk Factors: Results from Gothenburg Half Marathon,” Sports (Basel, Schweiz),
+årg. 8, nr 1, s. 2, dec. 2019, doi: 10.3390/sports8010002.
+
+        </li>
+        <li>
+        M. F. S. Bergeron, ”Heat Stress and Thermal Strain Challenges in Running,” Journal
+of Orthopaedic Sports Physical Therapy, årg. 44, nr 10, s. 831–838, okt. 2014, ”doi:
+10.1249/mss.0b013e31802ca597”.
+
+
+        </li>
+        <li>
         P. Höppe, ”The physiological equivalent temperature - a universal index for the
-biometeorological assessment of the thermal environment,” nternational Journal of
+biometeorological assessment of the thermal environment,” International Journal of
 Biometeorology, årg. 43, nr 2, s. 71–75, aug. 2011, doi: 10.1007/s004840050118.
+
+
         </li>
         <li>
-        S. Thorsson m. fl., ”Is Physiological Equivalent Temperature (PET) a superior
-screening tool for heat stress risk than Wet-Bulb Globe Temperature (WBGT)
-index? Eight years of data from the Gothenburg half marathon,” British Journal of
-Sports Medicine, årg. 55, nr 14, s. 825–830, Maj 2020, ”doi: 10.1136/bjsports-2019-
-100632”.
+       
+Y. Chen, ”Thermal indices for human biometeorology based on Python,” Sci Rep,
+årg. 13, nov. 2023, ”doi: 10.1038/s41598-023-47388-y”.
+
         </li>
         <li>
-        J. Lundberg m. fl., Två hjärtstopp och över 40 kollapser under Göteborgsvarvet,
-Göteborgs-Posten, 18 Maj 2024, [Online] Tillgänglig: https://www.gp.se/sport/tva-
-hjartstopp-och-over-40-kollapser-under-goteborgsvarvet-.bf8967e9-a236-4eac-846f-ce0cd7e2cfad,
-Hämtad: 2025-01-29.
-        </li>
-        <li>
-        M. Schwellnus m. fl., ”Prerace Medical Screening and Education Reduce Medical
-Encounters in Distance Road Races: SAFER VIII Study in 153 208 Race Star-
-ters,” British Journal of Sports Medicine, årg. 53, nr 10, s. 634–39, Maj. 2019, doi:
-https://doi.org/10.1136/bjsports-2018-099275.
-        </li>
-        <li>
-        H. Lüning, privat kommunikation, Feb. 2025.
-        </li>
+       
+        F. Lindberg m. fl., ”SOLWEIG 1.0 – Modelling spatial variations of 3D radiant fluxes
+and mean radiant temperature in complex urban settings,” International Journal of
+Biometeorology, årg. 52, s. 697–713, juni 2008, doi: 10.1007/s00484-008-0162-7.
+
+       
+               </li>
         <p className="thank-you-text">
         Vi vill rikta ett stort tack till Magnus Karlsten, Hampus Lüning och Fredrik Lindberg för deras stora engagemang och expertis inom området, 
         samt tacka Nils Bredin och Tilde Johansson för de fina bilderna. 
